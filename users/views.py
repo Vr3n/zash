d153e2bb10a8d63380ff219fb37300a7bddcd579
vr3n <vickyspatel@gmail.com>
2020-10-08 23:24:30 +0530

    -

--- users/views.py
+++ users/views.py
@@ -18,8 +18,8 @@ def register(request):
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}!')
             return redirect('login')
-	    else:
-	        form = UserRegisterForm()
+        else:
+            form = UserRegisterForm()
     return render(request, 'users/register.html',{'form':form, 'title':'Register'})
 
 # Function to View Profile
