9344c447e3faa15e9094f20dcc25c4f5d0aa106f
vr3n <vickyspatel@gmail.com>
2020-10-08 23:23:05 +0530

    -

--- users/views.py
+++ users/views.py
@@ -13,7 +13,7 @@ from .models import CustomUser
 def register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
-	    if form.is_valid():
+        if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}!')
