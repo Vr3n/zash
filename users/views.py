40a8cff2d9c141517bc44b4572a1892f30c1ebfe
vr3n <vickyspatel@gmail.com>
2020-10-08 23:04:23 +0530

    update

--- users/views.py
+++ users/views.py
@@ -13,13 +13,13 @@ from .models import CustomUser
 def register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
-    if form.is_valid():
-        form.save()
-        username = form.cleaned_data.get('username')
-        messages.success(request, f'Account created for {username}!')
-        return redirect('login')
-    else:
-        form = UserRegisterForm()
+	    if form.is_valid():
+        	form.save()
+	        username = form.cleaned_data.get('username')
+        	messages.success(request, f'Account created for {username}!')
+        	return redirect('login')
+	    else:
+	        form = UserRegisterForm()
     return render(request, 'users/register.html',{'form':form, 'title':'Register'})
 
 # Function to View Profile
